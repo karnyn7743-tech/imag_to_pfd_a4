daab970ce6ede9b7f9e8790a8d0dc9609102d0c4
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-09-17 01:04:22 +0300

    Clean up MainActivity by removing unused code
    Removed unused MethodChannel and related code for screen control.

--- android/app/src/kotlin/com/lanphone/app/MainActivity.kt
+++ android/app/src/kotlin/com/lanphone/app/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-
-    private val channelName = "com.lanphone.app/native"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,28 +20,7 @@ class MainActivity : FlutterActivity() {
                         or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
             )
         }
-    }
 
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        // قناة للتحكم في الشاشة من Dart
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            channelName
-        ).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "keepScreenOn" -> {
-                    val enable = call.argument<Boolean>("enable") ?: false
-                    if (enable) {
-                        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                    } else {
-                        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                    }
-                    result.success(true)
-                }
-                else -> result.notImplemented()
-            }
-        }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
