fa260e81e4128ab060a1c458812524fc743701e3
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-09-17 02:16:48 +0300

    Refactor settings.gradle.kts for Flutter integration
    Updated Flutter SDK path handling and plugin versions.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,14 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").let { file ->
-            if (file.exists()) {
-                file.inputStream().use { properties.load(it) }
-                properties.getProperty("flutter.sdk")
-            } else null
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
         }
-    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
         google()
@@ -18,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
